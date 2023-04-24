c512ee394494addcbec3d1afeba54e163aa9d86f
Vignesh A <127990092+VigneshA24@users.noreply.github.com>
2023-04-24 09:42:07 +0530

    Add files via upload

---
+++ forloops.js
@@ -0,0 +1,38 @@
+//3.JSON iterate overall for loops (for, for in, for of, forEach)
+
+var json = [
+    {
+        id: "vignesh", 
+        msg: "hello everyone",
+        task: "daily task",
+        mail: "avickyvignesh2001@gmail.com",
+    },
+    {
+        id: "vicky",
+        msg: "Welcome to all",
+        task: "day 3 task",
+        mail: "avickyvignesh2001@gmail.com",
+    }
+    ];
+
+    // for(let i=0;i<json.length;i++){
+    //     var obj= json[i];
+    //     console.log(obj.id);
+    //     console.log(obj.msg);
+    // }
+   
+
+    // for (let i in json) {
+    //       console.log(json[i].id);
+    //       //console.log(json[key].msg);
+    //     }
+
+    // json.forEach(function(obj){
+    //     console.log(obj.msg);
+    // })
+
+    // let text = "";
+    // for (let i of json[key].id) {
+    //  text += i; 
+    // }
+    //  console.log(text);
